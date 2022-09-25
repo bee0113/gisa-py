9d0831d877fe31a58d9ec7b6e7eb0450911c593e
mac <berno579@gmail.com>
2022-09-25 12:33:01 +0900

    수정

--- questionBank/Pro03.py
+++ questionBank/Pro03.py
@@ -1,6 +1,6 @@
 class CharClass:
     a = ['Seoul', 'Kyeongi', 'Inchon', 'Daejeon', 'Daegu', 'Pusan'];
-myVar = CharClass( )
+myVar = CharClass()
 str01 = ' '
 for i in myVar.a:
     str01 = str01 + i[0]
